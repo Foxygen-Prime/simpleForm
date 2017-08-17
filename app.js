d36f86b8025e10168f2d723b154c10bad26fdde9
Courtney Carlson <courtney.trochim@gmail.com>
2017-08-17 15:50:10 -0500

    homepage is working

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const mustacheExpress = require('mustache-express');
+// const bodyParser = require('body-parser');
+
 const app = express();
 
+app.engine('mustache', mustacheExpress());
+app.set('views', './views')
+app.set('view engine', 'mustache')
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get('/', function (req, res){
-  res.send('I am alive')
+  res.render('login')
 })
 
 app.listen(3000, function(){
