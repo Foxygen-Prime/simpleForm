649b837c9ef40d71a7c53cd7260328ff4092c960
Courtney Carlson <courtney.trochim@gmail.com>
2017-08-17 23:25:46 -0500

    definition issue unresvoled, line 47

--- app.js
+++ app.js
@@ -5,22 +5,35 @@ const session = require('express-session');
 
 const app = express();
 
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+
 app.engine('mustache', mustacheExpress());
 app.set('views', './views')
 app.set('view engine', 'mustache')
 
-const username_password = {
-  [
-    {user:jimminy
-    password:cricket },
-    {user:alfred
-    password:hitchcock },
-    {user:marlon
-    password:bradon },
-    {user:tupac
-    password:shakur },
-  ]
-}
+const username_password = [{
+    user: 'jimminy',
+    password: 'cricket'
+  },
+  {
+    user: 'alfred',
+    password: 'hitchcock'
+  },
+  {
+    user: 'marlon',
+    password: 'brando'
+  },
+  {
+    user: 'tupac',
+    password: 'shakur'
+  }
+]
+
+
 
 app.use(session({
   secret: 'keyboard cat',
@@ -28,36 +41,34 @@ app.use(session({
   saveUninitialized: true
 }))
 
-if (req.url == '/login') {
-  next()
-} else if (!sessions.login){
-  res.render('login')
-} else {
-  next()
-}
-
-
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use(function (req, res, next) {
-  next()
+app.use(function(req, res, next) {
+  if (req.url == '/login') {
+    next()
+  } else if (!req.sessions.username) {
+    res.render('login')
+  } else {
+    next()
+  }
 })
 
-app.get('/', function (req, res){
-    console.log('in home');
+app.get('/', function(req, res) {
+  console.log('in home');
   res.render('home')
 })
 
-app.post('/login', function (req, res) {
+app.post('/login', function(req, res) {
   console.log("username is " + req.body.username);
   console.log("password is " + req.body.password);
-  username_password.foreach(req.body.username === username_password.name &&& req.body.password === username_password.password) {
-res.render('home');
-} else {
-  res.render('home', {error: true})
-}
+  username_password.foreach(function(username_password) {
+    if (req.body.username === username_password.name && req.body.password === username_password.password) {
+      req.session.username = true;
+      res.render('home');
+    } else {
+      res.render('login', {error: true});
+    }
+  })
+})
 
-
-app.listen(3000, function(){
+app.listen(3000, function() {
   console.log('Successfully started the express application!');
 })
